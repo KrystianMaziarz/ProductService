6371e33b1f035155b21c3bf77bc0e1c984ce48a4
mazia <maziarz.krystian@gmail.com>
2020-02-12 18:45:36 +0100

    refactoring

--- src/main/java/pl/com/edge/productservice/controllers/ProductController.java
+++ src/main/java/pl/com/edge/productservice/controllers/ProductController.java
@@ -2,10 +2,9 @@ package pl.com.edge.productservice.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.com.edge.productservice.controllers.DTOS.ProductDTO;
 import pl.com.edge.productservice.services.ProductService;
-import pl.com.edge.productservice.model.Product;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -17,14 +16,11 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-    @GetMapping
-    public ResponseEntity <List<Product>> getAll() {
-        return ResponseEntity.ok(productService.findAll());
-    }
 
-    @PostMapping
-    public ResponseEntity <Product> getOneProduct (@RequestParam String name) {
-        Product productByName = productService.findProductByName(name);
+
+    @GetMapping ("/one")
+    public ResponseEntity <ProductDTO> getOneProduct (@RequestParam String name) {
+        ProductDTO productByName = productService.findProductByName(name);
         if (productByName!=null){
         return ResponseEntity.ok (productByName);
         }
--- src/main/java/pl/com/edge/productservice/services/ProductService.java
+++ src/main/java/pl/com/edge/productservice/services/ProductService.java
@@ -1,11 +1,12 @@
 package pl.com.edge.productservice.services;
 
 import org.springframework.stereotype.Service;
+import pl.com.edge.productservice.controllers.DTOS.ProductDTO;
 import pl.com.edge.productservice.model.Product;
-import pl.com.edge.productservice.model.Type;
 import pl.com.edge.productservice.repositories.ProductRepository;
 
-import java.util.List;
+import static pl.com.edge.productservice.services.DiscountPrice.setPriceAfterDiscount;
+import static pl.com.edge.productservice.services.ProductCounter.setCounterProduct;
 
 
 @Service
@@ -14,41 +15,20 @@ public class ProductService {
     private ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
-        this.productRepository=productRepository;
+        this.productRepository = productRepository;
     }
 
 
-
-    public Product findProductByName(String name) {
+    public ProductDTO findProductByName(String name) {
 
         for (Product product : productRepository.getProducts()) {
             if (product.getName().equalsIgnoreCase(name)) {
-               setPriceAfterDiscount(product);
-            product.setCounter(product.getCounter() + 1);
-            return product; }
+                setCounterProduct(product);
+                setPriceAfterDiscount(product);
+                return new ProductDTO(product);
+            }
         }
         return null;
     }
-    public List<Product> findAll() {
-        List<Product> products = productRepository.getProducts();
-        for (Product product : products){
-            setPriceAfterDiscount(product);
 
-        }
-        return products;
-    }
-    private void setPriceAfterDiscount (Product product) {
-
-        if (product.getType().equals(Type.FEMALE) || product.getType().equals(Type.MALE)) {
-            if (product.getPrice()!= null) {
-                product.setDiscountPrice(product.getPrice() * 0.95);
-            }
-        }
-        if (product.getType().equals(Type.KID)) {
-            if (product.getPrice() != null) {
-                product.setDiscountPrice(product.getPrice() * 0.90);
-            }
-        }
-
-    }
 }
